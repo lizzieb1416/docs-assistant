e32d2d0cfcd77a2f279909c48e66b516bf519450
lizzieb1416 <lisetbastidasg@gmail.com>
2024-05-20 16:25:22 +0200

    feat!: 🎉 cookiecutter scaffold

---
+++ src/common/app.py
@@ -0,0 +1,53 @@
+import logging
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+from . import config
+
+
+def _app(argA, argB):
+    return {
+        "argA": argA,
+        "argB": argB,
+    }
+
+
+def main():
+    import argparse
+
+    script_parser = argparse.ArgumentParser(
+        prog="app.py",
+        description="app.py script",
+    )
+
+    script_parser.add_argument(
+        "-a",
+        "--argA",
+        action="store",
+        metavar="arg1",
+        help="app.py argA",
+        required=True,
+    )
+
+    script_parser.add_argument(
+        "-b",
+        "--argB",
+        action="store",
+        metavar="color",
+        help="app.py color",
+        required=True,
+    )
+
+    result = _app.py(**vars(script_parser.parse_args()))
+
+    # Example how to access config toml file
+    logging.debug(config)
+
+    # Using **vars() to convert Namespace to dict
+    logging.debug(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
